de471db091f8658c2275ff2c848c3a379d1596b3
tomflynn1999 <tom@medshift.com>
2018-08-16 10:45:52 -0400

    completed time challenge

--- timechallenge.py
+++ timechallenge.py
@@ -16,8 +16,8 @@ import pytz
 
 available_zones = {'1': "Africa/Tunis",
                    '2': "Asia/Kolkata",
-                   '3': "Austrailian/Adelaide",
-                   '4': "Europse/Brussels",
+                   '3': "Austrailia/Adelaide",
+                   '4': "Europe/Brussels",
                    '5': "Europe/London",
                    '6': "Japan",
                    '7': "Pacific/Tahiti",
@@ -34,7 +34,12 @@ while True:
     if choice == '0':
         break
 
-        if choice in available_zones.keys():
-            tz_to_display = pytz.timezone(available_zones[choice])
-            world_time = datetime.datetime.now(tz=tz_to_display)
+    if choice in available_zones.keys():
+        tz_to_display = pytz.timezone(available_zones[choice])
+        world_time = datetime.datetime.now(tz=tz_to_display)
+        print("The time in {} is {} {}".format(available_zones[choice], world_time.strftime('%A %x %X %z'), world_time.tzname()))
+        print("Local time is {}".format(datetime.datetime.now().strftime('%A %x %X')))
+        print("UTC time is {}".format(datetime.datetime.utcnow().strftime('%A %x %X')))
+        print()
+
 
