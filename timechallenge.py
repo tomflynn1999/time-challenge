a45040d1c21c061f06084dba2abf2f41bc36d540
tomflynn1999 <tom@medshift.com>
2018-08-16 10:38:52 -0400

    made zones and began challenge

--- timechallenge.py
+++ timechallenge.py
@@ -14,3 +14,27 @@
 import datetime
 import pytz
 
+available_zones = {'1': "Africa/Tunis",
+                   '2': "Asia/Kolkata",
+                   '3': "Austrailian/Adelaide",
+                   '4': "Europse/Brussels",
+                   '5': "Europe/London",
+                   '6': "Japan",
+                   '7': "Pacific/Tahiti",
+                   '8': "US/Hawaii",
+                   '9': "Zulu"}
+
+print("Please choose a time zone (or 0 to quit):")
+for place in sorted(available_zones):
+    print("\t{}. {}".format(place, available_zones[place]))
+
+while True:
+    choice = input()
+
+    if choice == '0':
+        break
+
+        if choice in available_zones.keys():
+            tz_to_display = pytz.timezone(available_zones[choice])
+            world_time = datetime.datetime.now(tz=tz_to_display)
+
